f711ed56e4e89a3080af547acb0c7d92e08d4988
Daniel W. Robert <danielwrobert@gmail.com>
2021-09-02 17:14:50 -0400

    Add main navigation styles and toggle functionality.

--- functions.php
+++ functions.php
@@ -62,6 +62,8 @@ add_action( 'after_setup_theme', __NAMESPACE__ . '\\srf_setup' );
 function srf_scripts() {
 	// Theme styles
 	wp_enqueue_style( 'srf-style', get_template_directory_uri() . '/style.css', array(), wp_get_theme()->get( 'Version' ) );
+	// Navigation scripts
+	wp_enqueue_script( 'srf-navigation-js', get_template_directory_uri() . '/js/navigation.js', array(), wp_get_theme()->get( 'Version' ) );
 }
 add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\\srf_scripts' );
 
--- js/navigation.js
+++ js/navigation.js
@@ -5,131 +5,36 @@
  * navigation support for dropdown menus.
  */
 ( function() {
-	var container, button, menu, links, i, len, tocLinks;
+	// Burger menus
+	document.addEventListener( 'DOMContentLoaded', function() {
+		// open
+		const burger = document.querySelector( '.navbar-burger' );
+		const menu = document.querySelector( '.navbar-menu' );
 
-	container = document.getElementById( 'site-navigation' );
-	if ( ! container ) {
-		return;
-	}
-
-	button = container.getElementsByTagName( 'button' )[0];
-	if ( 'undefined' === typeof button ) {
-		return;
-	}
-
-	menu = container.getElementsByTagName( 'ul' )[0];
-
-	// Hide menu toggle button if menu is empty and return early.
-	if ( 'undefined' === typeof menu ) {
-		button.style.display = 'none';
-		return;
-	}
-
-	menu.setAttribute( 'aria-expanded', 'false' );
-	if ( -1 === menu.className.indexOf( 'nav-menu' ) ) {
-		menu.className += ' nav-menu';
-	}
-
-	button.onclick = function() {
-		if ( -1 !== container.className.indexOf( 'toggled' ) ) {
-			container.className = container.className.replace( ' toggled', '' );
-			button.setAttribute( 'aria-expanded', 'false' );
-			menu.setAttribute( 'aria-expanded', 'false' );
-		} else {
-			container.className += ' toggled';
-			button.setAttribute( 'aria-expanded', 'true' );
-			menu.setAttribute( 'aria-expanded', 'true' );
+		if ( null === menu ) {
+			return; // bail early
 		}
-	};
 
-	// Get all the link elements within the menu.
-	links    = menu.getElementsByTagName( 'a' );
-
-	// Each time a menu link is focused or blurred, toggle focus.
-	for ( i = 0, len = links.length; i < len; i++ ) {
-		links[i].addEventListener( 'focus', toggleFocus, true );
-		links[i].addEventListener( 'blur', toggleFocus, true );
-	}
-
-	/**
-	 * Handles smooth-scrolling behavior for TOC links
-	 */
-	// Get all the link elements within the menu.
-	tocLinks = document.querySelectorAll( '.toc--item a[href^="#"]' );
-
-	// Set the scrolling behavior from the TOC link to the target element to smooth.
-	if ( 'undefined' !== typeof tocLinks ) {
-		tocLinks.forEach( function( anchor ) {
-			anchor.addEventListener( 'click', function( e ) {
-				e.preventDefault();
-
-				let targetId = e.target.getAttribute( 'href' ).replace( '#', '' );
-				let targetElement = document.getElementById( targetId );
-
-				if ( ! targetElement ) {
-					return; // not available
-				}
-
-				scrollTo( {
-					top: targetElement.offsetTop,
-					behavior: 'smooth',
-				} );
+		if ( burger ) {
+			burger.addEventListener( 'click', function() {
+				menu.classList.toggle( 'hidden' );
 			} );
-		} );
-	}
-	
-
-
-	/**
-	 * Sets or removes .focus class on an element.
-	 */
-	function toggleFocus() {
-		var self = this;
-
-		// Move up through the ancestors of the current link until we hit .nav-menu.
-		while ( -1 === self.className.indexOf( 'nav-menu' ) ) {
-
-			// On li elements toggle the class .focus.
-			if ( 'li' === self.tagName.toLowerCase() ) {
-				if ( -1 !== self.className.indexOf( 'focus' ) ) {
-					self.className = self.className.replace( ' focus', '' );
-				} else {
-					self.className += ' focus';
-				}
-			}
-
-			self = self.parentElement;
 		}
-	}
 
-	/**
-	 * Toggles `focus` class to allow submenu access on tablets.
-	 */
-	( function( container ) {
-		var touchStartFn, i,
-			parentLink = container.querySelectorAll( '.menu-item-has-children > a, .page_item_has_children > a' );
+		// close
+		const close = document.querySelector( '.navbar-close' );
+		const backdrop = document.querySelector( '.navbar-backdrop' );
 
-		if ( 'ontouchstart' in window ) {
-			touchStartFn = function( e ) {
-				var menuItem = this.parentNode, i;
-
-				if ( ! menuItem.classList.contains( 'focus' ) ) {
-					e.preventDefault();
-					for ( i = 0; i < menuItem.parentNode.children.length; ++i ) {
-						if ( menuItem === menuItem.parentNode.children[i] ) {
-							continue;
-						}
-						menuItem.parentNode.children[i].classList.remove( 'focus' );
-					}
-					menuItem.classList.add( 'focus' );
-				} else {
-					menuItem.classList.remove( 'focus' );
-				}
-			};
-
-			for ( i = 0; i < parentLink.length; ++i ) {
-				parentLink[i].addEventListener( 'touchstart', touchStartFn, false );
-			}
+		if ( close ) {
+			close.addEventListener( 'click', function() {
+				menu.classList.toggle( 'hidden' );
+			} );
 		}
-	}( container ) );
+
+		if ( backdrop) {
+			backdrop.addEventListener( 'click', function() {
+				menu.classList.toggle( 'hidden' );
+			} );
+		}
+	} );
 } )();
