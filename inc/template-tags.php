814da48a76c35060875934f74e286587bbeebe96
Daniel W. Robert <danielwrobert@gmail.com>
2021-09-10 21:45:34 -0400

    Quickfix: Fix typo to resolve undefined fn error.

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -105,7 +105,7 @@ if ( ! function_exists( 'srf_post_thumbnail' ) ) :
 			return;
 		}
 
-		$srf_thumbnail_url = srf_get_the_post_thumbnail_url();
+		$srf_thumbnail_url = get_the_post_thumbnail_url();
 
 		echo '<img src="' . esc_url( $srf_thumbnail_url ) . '" alt="' . esc_attr( get_the_title() ) . '" srcset="' . esc_url( add_query_arg( 'w', 660, $srf_thumbnail_url ) ) . ' 660w, ' . esc_url( add_query_arg( 'w', 960, $srf_thumbnail_url ) ) . ' 960w, ' . esc_url( $srf_thumbnail_url ) . ' 1280w" sizes="(max-width: 768px) 100vw, 100vw"/>';
 	}
