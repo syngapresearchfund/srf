02b73f8011080b43609b2f17dc23fb7db136056e
Daniel W. Robert <danielwrobert@gmail.com>
2021-10-05 19:38:52 -0400

    Add people CPT single post template.

---
+++ single-srf-people.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @since 2019-05-19 First docBlock
+ * @package SRF
+ */
+
+namespace SRF;
+
+get_header();
+?>
+
+	<?php
+	while ( have_posts() ) :
+		the_post();
+
+		get_template_part( 'template-parts/content', 'single-srf-people' );
+
+		// If comments are open or we have at least one comment, load up the comment template.
+		if ( comments_open() || get_comments_number() ) :
+			comments_template();
+		endif;
+
+	endwhile; // End of the loop.
+	?>
+
+<?php
+get_footer();
