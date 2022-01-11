16e1075308fee689c3d234196907c51a5c66f8a3
Daniel W. Robert <danielwrobert@gmail.com>
2022-01-11 09:32:10 -0500

    Rename Research nav item back to Professionals for now.

--- template-parts/content-header.php
+++ template-parts/content-header.php
@@ -101,7 +101,7 @@ namespace SRF;
 					</li>
 					<li class="relative py-2 cursor-pointer" @click="openFourth = ! openFourth" @click.outside="openFourth = false">
 						<div class="text-gray-600 group rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-							<span>Research</span>
+							<span>Professionals</span>
 							<svg class="text-gray-600 ml-1 h-5 w-5 group-hover:text-gray-900" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
 								<path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
 							</svg>
