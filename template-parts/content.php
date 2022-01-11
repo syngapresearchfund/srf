5e5b84f62854a4966a2aef161f1931e894a8ea1c
Daniel W. Robert <danielwrobert@gmail.com>
2022-01-11 09:38:33 -0500

    Remove browser dots from post grid items.

--- template-parts/content.php
+++ template-parts/content.php
@@ -13,18 +13,10 @@ namespace SRF;
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class( 'col-span-2 border border-gray-700 rounded shadow-lg' ); ?>>
-	<header class="entry-header p-4 flex items-center justify-between border-b border-gray-700">
-		<div class="flex space-x-2 mr-10">
-			<div class="w-3 h-3 rounded-full bg-srf-blue-500"></div>
-			<div class="w-3 h-3 rounded-full bg-srf-purple-500"></div>
-			<div class="w-3 h-3 rounded-full bg-srf-green-500"></div>
-		</div>
-		<h3 class="font-semibold text-sm text-gray-600"><?php echo get_the_date( 'F j, Y' ); ?></h3>
-	</header><!-- .entry-header -->
-
 	<div class="text-gray-600">
-		<?php srf_post_thumbnail( 'w-full h-52 object-cover block' ); ?>
+		<?php srf_post_thumbnail( 'w-full h-52 object-cover block rounded-t' ); ?>
 		<div class="p-6">
+			<h4 class="font-semibold text-sm text-gray-600"><?php echo get_the_date( 'F j, Y' ); ?></h4>
 			<?php the_title( '<h2 class="entry-title mb-2 font-bold text-lg text-gray-700"><a class="hover:underline" href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' ); ?>
 			<?php the_excerpt(); ?>
 		</div>
