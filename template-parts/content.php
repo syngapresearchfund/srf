d8d3970b00a4a28a38126e6efc9ce2593f45e317
Daniel W. Robert <danielwrobert@gmail.com>
2022-01-21 13:19:06 -0500

    Update post archive cards.

--- template-parts/content.php
+++ template-parts/content.php
@@ -14,11 +14,13 @@ namespace SRF;
 
 <article id="post-<?php the_ID(); ?>" <?php post_class( 'col-span-2 border border-gray-700 rounded shadow-lg' ); ?>>
 	<div class="text-gray-600">
-		<?php srf_post_thumbnail( 'w-full h-52 object-cover block rounded-t' ); ?>
-		<div class="p-6">
-			<h4 class="font-semibold text-sm text-gray-600"><?php echo get_the_date( 'F j, Y' ); ?></h4>
-			<?php the_title( '<h2 class="entry-title mb-2 font-bold text-lg text-gray-700"><a class="hover:underline" href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' ); ?>
-			<?php the_excerpt(); ?>
-		</div>
+		<a class="link__more font-semibold hover:underline" href="<?php esc_url( get_permalink() ); ?>" rel="bookmark">
+			<?php srf_post_thumbnail( 'w-full h-52 object-cover block rounded-t' ); ?>
+			<div class="p-6">
+				<h4 class="font-semibold text-sm text-gray-600"><?php echo get_the_date( 'F j, Y' ); ?></h4>
+				<?php the_title( '<h2 class="entry-title mb-2 font-bold text-lg text-gray-700">', '</h2>' ); ?>
+				<p class="mt-4">View article →</p>
+			</div>
+		</a>
 	</div>
 </article><!-- #post-<?php the_ID(); ?> -->
