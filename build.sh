5d7fdf273c3273ecb9e36e56e24fa35db1e6e2c6
Daniel W. Robert <danielwrobert@gmail.com>
2022-01-30 11:36:29 -0500

    Rename /dist output directory.
    Name dist dir to srf to reflect the theme name when uploaded.
    
    Rename the exclude file to build-exclude to be a bit more generic.

--- build.sh
+++ build.sh
@@ -6,23 +6,23 @@ YELLOW="\033[1;33m"
 NOCOLOR="\033[0m"
 currentdir=${PWD##*/}
 
-if [ ! -d ./dist ]; then
-	echo "The /dist directory was not found. Creating directory..."
-	mkdir dist
+if [ ! -d ./srf ]; then
+	echo "The /srf directory was not found. Creating directory..."
+	mkdir srf
 fi
 
-if [ ! -f ./dist-exclude.txt ]; then
+if [ ! -f ./build-exclude.txt ]; then
 	echo "ERROR: You must have the exclude file present in the this directory."
 	exit 1
 fi
 
-echo "${GREEN}Syncing dist files...${NOCOLOR}"
-rm -r ./dist/*
+echo "${GREEN}Syncing srf files...${NOCOLOR}"
+rm -r ./srf/*
 rm ./srf-theme.zip
-rsync -avzu --delete --progress -h --exclude-from="dist-exclude.txt" ./* ./dist
+rsync -avzu --delete --progress -h --exclude-from="build-exclude.txt" ./* ./srf
 
 echo "${GREEN}Syncing complete.${NOCOLOR}"
 echo "${GREEN}Building zip file...${NOCOLOR}"
-zip -r ./srf-theme.zip ./dist
+zip -r ./srf-theme.zip ./srf
 
 echo "${GREEN}Complete! Upload the ./srf-theme.zip file to your Themes in wp-admin.${NOCOLOR}"
