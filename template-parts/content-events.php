c3b74ae38d42ce999f100d5c4e5486e5db9c1af5
Daniel W. Robert <danielwrobert@gmail.com>
2022-01-30 00:06:28 -0500

    Add featured img to Events archive.

--- template-parts/content-events.php
+++ template-parts/content-events.php
@@ -12,7 +12,8 @@ namespace SRF;
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class( 'p-6 bg-gray-200 even:bg-opacity-50 flex justify-between items-center first:rounded-t-lg last:rounded-b-lg' ); ?>>
-	<?php the_title( '<h2 class="entry-title w-full sm:w-4/6 text-2xl font-bold text-center sm:text-left"><a class="hover:underline" href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' ); ?>
-	<h3 class="hidden sm:block text-sm text-right font-normal"><?php echo get_the_date( 'F j, Y' ); ?></h3>
+<article id="post-<?php the_ID(); ?>" <?php post_class( 'm-3 bg-gray-200 even:bg-opacity-50 flex justify-between items-center' ); ?>>
+	<?php srf_post_thumbnail( 'w-40 h-28 mr-6 object-cover block' ); ?>
+	<?php the_title( '<h2 class="entry-title w-full sm:w-4/6 mr-6 text-2xl font-bold text-center sm:text-left"><a class="hover:underline" href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' ); ?>
+	<h3 class="hidden mr-6 sm:block text-sm text-right font-normal"><?php echo get_the_date( 'F j, Y' ); ?></h3>
 </article>
