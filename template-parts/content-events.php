fe2d8121d89c9f3887bebc11cc271405fe7ac39c
Daniel W. Robert <danielwrobert@gmail.com>
2022-02-06 13:20:13 -0500

    Adjust Events layout on mobile.

--- template-parts/content-events.php
+++ template-parts/content-events.php
@@ -12,8 +12,8 @@ namespace SRF;
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class( 'm-3 bg-gray-200 even:bg-opacity-50 flex justify-between items-center' ); ?>>
-	<?php srf_post_thumbnail( 'w-40 h-28 mr-6 object-cover block' ); ?>
-	<?php the_title( '<h2 class="entry-title w-full sm:w-4/6 mr-6 text-2xl font-bold text-center sm:text-left"><a class="hover:underline" href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' ); ?>
-	<h3 class="hidden mr-6 sm:block text-sm text-right font-normal"><?php echo get_the_date( 'F j, Y' ); ?></h3>
+<article id="post-<?php the_ID(); ?>" <?php post_class( 'm-3 bg-gray-200 even:bg-opacity-50 xs:flex justify-between items-center' ); ?>>
+	<?php srf_post_thumbnail( 'block w-full h-48 xs:w-40 xs:h-28 mx-auto xs:mx-0 xs:mr-6 object-cover' ); ?>
+	<?php the_title( '<h2 class="entry-title w-full xs:w-4/6 mr-6 p-3 xs:p-0 text-2xl font-bold text-center xs:text-left"><a class="hover:underline" href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' ); ?>
+	<h3 class="hidden sm:block mr-6 text-sm text-right font-normal"><?php echo get_the_date( 'F j, Y' ); ?></h3>
 </article>
