d82334308597ab6619fc3be3e10ce2246dbeec68
Daniel W. Robert <danielwrobert@gmail.com>
2022-02-06 20:50:52 -0500

    Add line-clamp to Event headers.

--- template-parts/content-events.php
+++ template-parts/content-events.php
@@ -14,6 +14,6 @@ namespace SRF;
 
 <article id="post-<?php the_ID(); ?>" <?php post_class( 'm-3 bg-gray-200 even:bg-opacity-50 xs:flex justify-between items-center' ); ?>>
 	<?php srf_post_thumbnail( 'block w-full h-48 xs:w-40 xs:h-28 mx-auto xs:mx-0 xs:mr-6 object-cover' ); ?>
-	<?php the_title( '<h2 class="entry-title w-full xs:w-4/6 mr-6 p-3 xs:p-0 text-2xl font-bold text-center xs:text-left"><a class="hover:underline" href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' ); ?>
+	<?php the_title( '<h2 class="entry-title w-full xs:w-4/6 mr-6 p-3 xs:p-0 text-2xl font-bold text-center xs:text-left xs:line-clamp-2 md:line-clamp-none"><a class="hover:underline" href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' ); ?>
 	<h3 class="hidden sm:block mr-6 text-sm text-right font-normal"><?php echo get_the_date( 'F j, Y' ); ?></h3>
 </article>
