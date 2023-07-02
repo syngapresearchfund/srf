ee5dc415bb8a7fa265278326dfbba2ab5895ca14
Daniel W. Robert <danielwrobert@gmail.com>
2023-07-02 11:22:24 -0400

    Move searchform markup for main nav to fix stacking order / z-index.

--- template-parts/content-header.php
+++ template-parts/content-header.php
@@ -32,6 +32,19 @@ namespace SRF;
 				</button>
 			</div>
 			<!-- Desktop menu, show/hide dropdown items on item state. -->
+			<div class="hidden xl:flex justify-end items-stretch absolute right-0 w-96" x-data="{ searchOpen: false, searchClosed: true }">
+				<form id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" method="GET" class="w-full absolute -bottom-full right-0">
+					<input type="text" name="s" placeholder="<?php esc_attr_e( 'Search...' ); ?>" value="<?php the_search_query(); ?>" class="w-full h-10 p-2 outline-none border-2 border-srf-purple-700 focus:border-srf-purple-800 placeholder-gray-400 text-purple-900" x-show="searchOpen" x-transition>
+				</form>
+				<button class="inline-block p-2 bg-srf-purple-700 hover:bg-srf-purple-800 text-white" @click="searchOpen = ! searchOpen; searchClosed = ! searchClosed">
+					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6" x-show="searchClosed">
+						<path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+					</svg>
+					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6" x-show="searchOpen">
+						<path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+					</svg>
+				</button>
+			</div>
 			<nav class="hidden xl:block font-sans">
 				<ul class="flex space-x-6">
 				<?php
@@ -119,24 +132,6 @@ namespace SRF;
 				?>
 				</ul>
 			</nav>
-			<div class="hidden xl:flex justify-end items-stretch absolute right-0 w-96" x-data="{ searchOpen: false, searchClosed: true }">
-				<form id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" method="GET" class="w-full absolute -bottom-full right-0">
-					<input type="text" name="s" placeholder="<?php esc_attr_e( 'Search...' ); ?>" value="<?php the_search_query(); ?>" class="w-full h-10 p-2 outline-none border-2 border-srf-purple-700 focus:border-srf-purple-800 placeholder-gray-400 text-purple-900" x-show="searchOpen" x-transition>
-				</form>
-				<button class="inline-block p-2 bg-srf-purple-700 hover:bg-srf-purple-800 text-white" @click="searchOpen = ! searchOpen; searchClosed = ! searchClosed">
-					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6" x-show="searchClosed">
-						<path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-					</svg>
-					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6" x-show="searchOpen">
-						<path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-					</svg>
-				</button>
-				<!-- <button class="inline-block p-3 bg-srf-purple-700 hover:bg-srf-purple-800 text-white" @click="searchClosed = ! searchClosed">
-					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6" x-show="searchClosed">
-						<path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-					</svg>
-				</button> -->
-			</div>
 		</div>
 	</div>
 </div>
