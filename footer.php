8d7628eb8b378f0d41708704f613ac7bb0f999b0
Daniel W. Robert <danielwrobert@gmail.com>
2023-07-01 22:28:11 -0400

    Add external seal links to open in new tab.

--- footer.php
+++ footer.php
@@ -37,8 +37,8 @@ namespace SRF;
 					<p class="text-lg m-3">The Syngap Research fund is a 501(c)(3) public charity (EIN <a class="hover:underline text-srf-purple-100" href="<?php echo esc_url( get_theme_file_uri( 'assets/files/SRF-501c3-Ruling.pdf' ) ); ?>">83-1200789</a>) headquartered in California.</p>
 					<ul class="flex space-x-2 justify-end place-items-center">
 						<li class="w-32"><a href="<?php echo esc_url( home_url( '/syngap10-podcast/' ) ); ?>"><img src="<?php echo esc_url( get_theme_file_uri( 'assets/images/tdc-badge-podcast.png' ) ); ?>" alt="The Best RARE Podcasts"></a></li>
-						<li class="w-32"><a href="https://globalgenes.org/"><img src="<?php echo esc_url( get_theme_file_uri( 'assets/images/global-advocacy-alliance-founding-member-badge.png' ) ); ?>" alt="Global Advocacy Alliance Founding Member"></a></li>
-						<li class="w-24"><a href="https://www.guidestar.org/profile/83-1200789"><img src="<?php echo esc_url( get_theme_file_uri( 'assets/images/seal-of-transparency-2022.png' ) ); ?>" alt="Guidestar Platinum Symbol of Transparency 2020"></a></li>
+						<li class="w-32"><a href="https://globalgenes.org/" target="_blank" rel="noopener noreferrer"><img src="<?php echo esc_url( get_theme_file_uri( 'assets/images/global-advocacy-alliance-founding-member-badge.png' ) ); ?>" alt="Global Advocacy Alliance Founding Member"></a></li>
+						<li class="w-24"><a href="https://www.guidestar.org/profile/83-1200789" target="_blank" rel="noopener noreferrer"><img src="<?php echo esc_url( get_theme_file_uri( 'assets/images/seal-of-transparency-2022.png' ) ); ?>" alt="Guidestar Platinum Symbol of Transparency 2020"></a></li>
 						<li class="w-32"><a href="<?php echo esc_url( home_url( '/gnp/' ) ); ?>"><img src="<?php echo esc_url( get_theme_file_uri( 'assets/images/2022-top-rated-awards-badge.png' ) ); ?>" alt="2022 Top Rated Awards"></a></li>
 					</ul>
 				</div>
