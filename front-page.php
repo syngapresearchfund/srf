34addbea0882eeb02db2ba8256857a65f9b464ea
Daniel W. Robert <danielwrobert@gmail.com>
2023-10-06 22:36:20 -0400

    Update Warriors archive link on home page.

--- front-page.php
+++ front-page.php
@@ -234,7 +234,7 @@ get_header();
 		?>
 
 		<!-- big button / cta -->
-		<a href="<?php echo esc_url( home_url( '/warriors/' ) ); ?>"
+		<a href="<?php echo esc_url( home_url( '/syngap-warriors/' ) ); ?>"
 		   class="font-sans inline-flex bg-srf-blue-500 hover:bg-srf-blue-800 rounded py-3 px-8 text-white transition duration-500 font-bold">View
 			all warriors
 			<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 inline-block" fill="none" viewBox="0 0 24 24"
