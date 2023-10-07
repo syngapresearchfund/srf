d73c9e05c897c9ec80e092ba85ca1d3272df646b
Daniel W. Robert <danielwrobert@users.noreply.github.com>
2023-10-06 21:32:21 -0400

    Remove featured event label from homepage evt grid. (#19)

--- template-parts/content-events-grid.php
+++ template-parts/content-events-grid.php
@@ -19,7 +19,6 @@ $date_size      = $args['current_item'] > 0 ? '' : 'xl:text-base';
 <article id="post-<?php the_ID(); ?>" <?php post_class( 'shadow-lg bg-gray-300 bg-opacity-50 xs:flex items-center ' . $col_span ); ?>>
 	<?php srf_post_thumbnail( 'block w-full h-48 xs:w-56 mx-auto border-8 border-gray-300 object-cover ' . $thumbnail_size ); ?>
 	<div class="w-full xs:w-4/6 px-6 text-center xs:text-left">
-		<?php echo $args['current_item'] === 0 ? '<h4 class="mb-2 font-extrabold uppercase"><span class="text-transparent bg-clip-text bg-gradient-to-r from-srf-purple-500 to-srf-blue-500">Featured</span><span class="text-transparent bg-clip-text bg-gradient-to-r from-srf-blue-500 via-srf-green-600 to-srf-green-500"> Event</span></h4>' : ''; ?>
 		<?php the_title( '<h2 class="entry-title text-2xl font-bold xs:line-clamp-2 md:line-clamp-3 ' . $title_size . '"><a class="hover:underline" href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' ); ?>
 	</div>
 	<?php if ( has_excerpt() ) : ?>
