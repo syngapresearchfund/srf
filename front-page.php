ec3e689fe29fdbcd3f1fef3a8c60bc07f2537679
Daniel W. Robert <danielwrobert@gmail.com>
2023-11-12 22:56:34 -0500

    Adjust text colors away from lighter gray.

--- front-page.php
+++ front-page.php
@@ -36,21 +36,21 @@ get_header();
 		<h2 class="mb-10 text-3xl lg:text-4xl font-extrabold text-gray-600">Newly Diagnosed?</h2>
 		<div class="max-w-6xl mx-auto lg:grid grid-cols-3 gap-16 space-y-16 lg:space-y-0 mb-10 text-center">
 			<div class="flex flex-col items-center space-y-6">
-				<h3 class="text-gray-500 text-2xl font-bold text-center underline">Join our registry</h3>
+				<h3 class="text-gray-600 text-2xl font-bold text-center underline">Join our registry</h3>
 				<p class="flex-auto">Register with Ciitizen to collect as much de-identified group data for researchers
 					to use. We need as many to register to improve the chances for successful clinical trials.</p>
 				<a href="<?php echo esc_url( home_url( '/join-the-ciitizen-registry/' ) ); ?>"
 				   class="font-sans inline-block py-3 px-12 rounded hover:bg-srf-purple-500 border-2 border-srf-purple-500 text-srf-purple-500 hover:text-white font-bold text-lg tracking-wide transition duration-500">Register</a>
 			</div>
 			<div class="flex flex-col items-center space-y-6">
-				<h3 class="text-gray-500 text-2xl font-bold text-center underline">Get involved</h3>
+				<h3 class="text-gray-600 text-2xl font-bold text-center underline">Get involved</h3>
 				<p class="flex-auto">The best way to help someone with SYNGAP1 is to help spread awareness and stay
 					informed. Start by connecting with SRF, holding a fundraiser, and becoming a volunteer.</p>
 				<a href="<?php echo esc_url( home_url( '/get-involved/' ) ); ?>"
 				   class="font-sans inline-block py-3 px-12 rounded hover:bg-srf-blue-500 border-2 border-srf-blue-500 text-srf-blue-500 hover:text-white font-bold text-lg tracking-wide transition duration-500">Connect</a>
 			</div>
 			<div class="flex flex-col items-center space-y-6">
-				<h3 class="text-gray-500 text-2xl font-bold text-center underline">New families</h3>
+				<h3 class="text-gray-600 text-2xl font-bold text-center underline">New families</h3>
 				<p class="flex-auto">If you're new to SYNGAP1 or SRF, we have developed a 'Welcome Pack' with basic
 					information, resources, as well as important tools for you and your doctor.</p>
 				<a href="<?php echo esc_url( home_url( '/syngap1-resources-for-newly-diagnosed-families/' ) ); ?>"
@@ -299,28 +299,28 @@ get_header();
 		<div class="max-w-6xl mx-auto lg:grid grid-cols-3 gap-16 space-y-16 lg:space-y-0 mb-10 text-center">
 			<div>
 				<h2 class="text-5xl text-srf-blue-500 font-bold">$4.45M</h2>
-				<h3 class="text-lg text-gray-500 font-normal">Funds committed</h3>
+				<h3 class="text-lg text-gray-600 font-normal">Funds committed</h3>
 			</div>
 			<div>
 				<h2 class="text-5xl text-srf-blue-500 font-bold">1297</h2>
-				<h3 class="text-lg text-gray-500 font-normal">Patients counted</h3>
+				<h3 class="text-lg text-gray-600 font-normal">Patients counted</h3>
 			</div>
 			<div>
 				<h2 class="text-5xl text-srf-blue-500 font-bold">139</h2>
-				<h3 class="text-lg text-gray-500 font-normal">Families volunteering</h3>
+				<h3 class="text-lg text-gray-600 font-normal">Families volunteering</h3>
 			</div>
 			<div>
 				<h2 class="text-5xl text-srf-blue-500 font-bold">214</h2>
-				<h3 class="text-lg text-gray-500 font-normal">US Families on our Ciitizen NHS</h3>
+				<h3 class="text-lg text-gray-600 font-normal">US Families on our Ciitizen NHS</h3>
 			</div>
 			<div>
 				<h2 class="text-5xl text-srf-blue-500 font-bold">35</h2>
-				<h3 class="text-lg text-gray-500 font-normal">Companies & institutes working on a genetic therapy for
+				<h3 class="text-lg text-gray-600 font-normal">Companies & institutes working on a genetic therapy for
 					SYNGAP1</h3>
 			</div>
 			<div>
 				<h2 class="text-5xl text-srf-blue-500 font-bold">196</h2>
-				<h3 class="text-lg text-gray-500 font-normal">Patients profiled</h3>
+				<h3 class="text-lg text-gray-600 font-normal">Patients profiled</h3>
 			</div>
 		</div>
 	</div>
