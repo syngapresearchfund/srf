3aa9b6b67153eb7a28aae5a44d4f01fa9927057e
Daniel W. Robert <danielwrobert@users.noreply.github.com>
2023-11-14 20:55:42 -0500

    Add MailChimp Connection (#22)
    * Add action method to form pointing to MC.
    
    * Connect and style MailChimp form.

--- footer.php
+++ footer.php
@@ -19,46 +19,10 @@ namespace SRF;
 <!-- <footer class="bg-gradient-to-b from-gray-100 to-gray-300 text-purple-100"> -->
 <!-- <footer class="bg-gradient-to-b from-purple-500 to-purple-900 text-purple-100"> -->
 <footer class="bg-srf-purple-500 text-white border-t-4 border-srf-purple-light">
-	<div class="container mx-auto px-6 lg:px-0 py-24">
-		<!-- top footer -->
-		<div class="lg:flex justify-between gap-x-28 space-y-12 lg:space-y-0">
-			<!-- newsletter -->
-			<div class="flex flex-col flex-1 justify-center">
-				<!-- <h4 class="mb-4 font-bold text-2xl lg:text-5xl text-transparent bg-clip-text bg-gradient-to-r from-teal-400 to-purple-500">Get the latest updates!</h4> -->
-				<h4 class="mb-4 font-bold text-2xl lg:text-4xl text-center lg:text-left">Sign up for our
-					newsletter!</h4>
-				<p class="mb-3 text-center lg:text-left">Keep up-to-date with helpful resources, inspiring stories, and
-					the latest SRF news.</p>
-				<form action="" method="POST" class="flex">
-					<input type="email" name="email" placeholder="email@example.com"
-						   class="w-full xl:w-2/3 p-3 rounded-l outline-none border-2 border-r-0 border-srf-purple-700 focus:border-srf-purple-800 placeholder-srf-purple-400 text-purple-900">
-					<button class="p-3 bg-srf-purple-700 hover:bg-srf-purple-800 text-white rounded-r">Submit</button>
-				</form>
-			</div>
-			<!-- links -->
-			<div class="flex-1 text-center lg:text-right">
-				<p class="text-lg m-3">The Syngap Research fund is a 501(c)(3) public charity (EIN <a
-						class="hover:underline text-srf-purple-100"
-						href="<?php echo esc_url( get_theme_file_uri( 'assets/files/SRF-501c3-Ruling.pdf' ) ); ?>">83-1200789</a>)
-					headquartered in California.</p>
-				<ul class="flex space-x-2 justify-center xl:justify-end place-items-center">
-					<li class="w-32"><a href="<?php echo esc_url( home_url( '/syngap10-podcast/' ) ); ?>"><img
-								src="<?php echo esc_url( get_theme_file_uri( 'assets/images/tdc-badge-podcast.png' ) ); ?>"
-								alt="The Best RARE Podcasts"></a></li>
-					<li class="w-32"><a href="https://globalgenes.org/" target="_blank" rel="noopener noreferrer"><img
-								src="<?php echo esc_url( get_theme_file_uri( 'assets/images/global-advocacy-alliance-founding-member-badge.png' ) ); ?>"
-								alt="Global Advocacy Alliance Founding Member"></a></li>
-					<li class="w-24"><a href="https://www.guidestar.org/profile/83-1200789" target="_blank"
-										rel="noopener noreferrer"><img
-								src="<?php echo esc_url( get_theme_file_uri( 'assets/images/seal-of-transparency-2022.png' ) ); ?>"
-								alt="Guidestar Platinum Symbol of Transparency 2020"></a></li>
-					<li class="w-32"><a href="<?php echo esc_url( home_url( '/gnp/' ) ); ?>"><img
-								src="<?php echo esc_url( get_theme_file_uri( 'assets/images/2023-top-rated-awards-badge.png' ) ); ?>"
-								alt="2022 Top Rated Awards"></a></li>
-				</ul>
-			</div>
-		</div>
-	</div>
+
+	<!-- subfooter -->
+	<?php get_template_part( 'template-parts/content', 'subfooter' ); ?>
+
 	<!-- bottom footer -->
 	<div class="pt-4 pb-12 bg-gray-900 text-gray-100 text-sm text-center md:text-left space-y-4 md:space-y-0">
 		<div
--- template-parts/content-subfooter.php
+++ template-parts/content-subfooter.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template part for subfooter CTA block
+ * Template part for subfooter block
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
@@ -12,19 +12,56 @@ namespace SRF;
 
 ?>
 
-<section class="wpcom-go-footer__subfooter">
-	<div class="section-content">
-		<div class="subfooter-cta-text">
-			<h2 class="subfooter-cta-bg-text">
-				<?php esc_html_e( 'Design your portfolio.' ); ?>
-				<span><?php esc_html_e( 'Open a store.' ); ?></span>
-				<span><?php esc_html_e( 'Launch a business.' ); ?></span>
-			</h2>
-			<h2>
-				<?php esc_html_e( 'You can. You will.' ); ?>
-				<span><?php esc_html_e( 'We’ll help.' ); ?></span>
-			</h2>
-			<p class="no-widows"><?php esc_html_e( 'Invent the world’s greatest cat food, save a rainforest, start a needlepoint club. Whatever it is, it’s going to need a website—that’s where we come in.' ); ?></p>
+	<div class="container mx-auto px-6 lg:px-0 py-24">
+		<!-- top footer -->
+		<div class="lg:flex justify-between gap-x-28 space-y-12 lg:space-y-0">
+			<!-- newsletter -->
+			<div id="mc_embed_shell">
+				<div id="mc_embed_signup">
+					<div class="flex flex-col flex-1 justify-center">
+						<h4 class="mb-4 font-bold text-2xl lg:text-4xl text-center lg:text-left">Sign up for our newsletter!</h4>
+						<p class="mb-3 text-center lg:text-left">Keep up-to-date with helpful resources, inspiring stories, and the latest SRF news.</p>
+						<form action="https://syngapresearchfund.us4.list-manage.com/subscribe/post?u=5b5957867c282184c537770df&amp;id=001d463f12&amp;f_id=007fc5e9f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="flex validate" target="_blank">
+							<div id="mc_embed_signup_scroll" class="w-full">
+								<input type="email" name="EMAIL" placeholder="email@example.com" id="mce-EMAIL" class="required email w-full p-3 rounded-l outline-none border-2 border-r-0 border-srf-purple-700 focus:border-srf-purple-800 placeholder-srf-purple-400 text-purple-900" required="" value="">
+								<div aria-hidden="true" style="position: absolute; left: -5000px;">
+									<input type="text" name="b_5b5957867c282184c537770df_001d463f12" tabindex="-1" value="">
+								</div>
+							</div>
+							<button name="subscribe" id="mc-embedded-subscribe" class="p-3 bg-srf-purple-700 hover:bg-srf-purple-800 text-white rounded-r">Submit</button>
+						</form>
+						<div id="mce-responses" class="clear">
+							<div class="response" id="mce-error-response" style="display: none;"></div>
+							<div class="response" id="mce-success-response" style="display: none;"></div>
+						</div>
+					</div>
+					<script type="text/javascript" src="//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js"></script>
+					<script type="text/javascript">
+						(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='MMERGE1';ftypes[1]='text';fnames[2]='MMERGE2';ftypes[2]='text';fnames[3]='MMERGE3';ftypes[3]='address';fnames[4]='MMERGE4';ftypes[4]='address';fnames[5]='MMERGE5';ftypes[5]='text';fnames[6]='MMERGE6';ftypes[6]='text';fnames[7]='MMERGE7';ftypes[7]='text';fnames[8]='MMERGE8';ftypes[8]='text';fnames[9]='MMERGE9';ftypes[9]='text';}(jQuery));var $mcj = jQuery.noConflict(true);
+					</script>
+				</div>
+			</div>
+			<!-- links -->
+			<div class="flex-1 text-center lg:text-right">
+				<p class="text-lg m-3">The Syngap Research fund is a 501(c)(3) public charity (EIN <a
+						class="hover:underline text-srf-purple-100"
+						href="<?php echo esc_url( get_theme_file_uri( 'assets/files/SRF-501c3-Ruling.pdf' ) ); ?>">83-1200789</a>)
+					headquartered in California.</p>
+				<ul class="flex space-x-2 justify-center xl:justify-end place-items-center">
+					<li class="w-32"><a href="<?php echo esc_url( home_url( '/syngap10-podcast/' ) ); ?>"><img
+								src="<?php echo esc_url( get_theme_file_uri( 'assets/images/tdc-badge-podcast.png' ) ); ?>"
+								alt="The Best RARE Podcasts"></a></li>
+					<li class="w-32"><a href="https://globalgenes.org/" target="_blank" rel="noopener noreferrer"><img
+								src="<?php echo esc_url( get_theme_file_uri( 'assets/images/global-advocacy-alliance-founding-member-badge.png' ) ); ?>"
+								alt="Global Advocacy Alliance Founding Member"></a></li>
+					<li class="w-24"><a href="https://www.guidestar.org/profile/83-1200789" target="_blank"
+										rel="noopener noreferrer"><img
+								src="<?php echo esc_url( get_theme_file_uri( 'assets/images/seal-of-transparency-2022.png' ) ); ?>"
+								alt="Guidestar Platinum Symbol of Transparency 2020"></a></li>
+					<li class="w-32"><a href="<?php echo esc_url( home_url( '/gnp/' ) ); ?>"><img
+								src="<?php echo esc_url( get_theme_file_uri( 'assets/images/2023-top-rated-awards-badge.png' ) ); ?>"
+								alt="2022 Top Rated Awards"></a></li>
+				</ul>
+			</div>
 		</div>
 	</div>
-</section>
