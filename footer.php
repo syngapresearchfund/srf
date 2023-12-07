dc5a83423d2ae348619e6bd519631275e49c9b19
Daniel W. Robert <danielwrobert@gmail.com>
2023-12-06 21:36:14 -0500

    Add credit comment for SVG flags.

--- footer.php
+++ footer.php
@@ -16,8 +16,6 @@ namespace SRF;
 
 </main><!-- #content -->
 
-<!-- <footer class="bg-gradient-to-b from-gray-100 to-gray-300 text-purple-100"> -->
-<!-- <footer class="bg-gradient-to-b from-purple-500 to-purple-900 text-purple-100"> -->
 <footer class="bg-srf-purple-500 text-white border-t-4 border-srf-purple-light">
 
 	<!-- subfooter -->
@@ -27,7 +25,7 @@ namespace SRF;
 	<div class="pt-4 pb-12 bg-gray-900 text-gray-100 text-sm text-center md:text-left space-y-4 md:space-y-0">
 		<div
 			class="container mx-auto px-6 lg:px-0 space-y-4 md:space-y-0 flex flex-wrap justify-center md:justify-between">
-			<!-- flags -->
+			<!-- flags (credit: https://github.com/HatScripts/circle-flags) -->
 			<div class="flex space-x-5">
 				<a href="<?php echo esc_url( home_url( '/team/board-members/' ) ); ?>" class="block transition duration-500 ease-in-out filter grayscale hover:grayscale-0">
 					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 512 512"><mask id="a"><circle cx="256" cy="256" r="256" fill="#fff"/></mask><g mask="url(#a)"><path fill="#eee" d="M256 0h256v64l-32 32 32 32v64l-32 32 32 32v64l-32 32 32 32v64l-256 32L0 448v-64l32-32-32-32v-64z"/><path fill="#d80027" d="M224 64h288v64H224Zm0 128h288v64H256ZM0 320h512v64H0Zm0 128h512v64H0Z"/><path fill="#0052b4" d="M0 0h256v256H0Z"/><path fill="#eee" d="m187 243 57-41h-70l57 41-22-67zm-81 0 57-41H93l57 41-22-67zm-81 0 57-41H12l57 41-22-67zm162-81 57-41h-70l57 41-22-67zm-81 0 57-41H93l57 41-22-67zm-81 0 57-41H12l57 41-22-67Zm162-82 57-41h-70l57 41-22-67Zm-81 0 57-41H93l57 41-22-67zm-81 0 57-41H12l57 41-22-67Z"/></g></svg>
