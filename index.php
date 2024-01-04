c61a6f8aa941adb53bd1430c54eb9cfc4c923e87
Daniel W. Robert <danielwrobert@gmail.com>
2024-01-03 20:45:04 -0500

    Add archive subheading.

--- index.php
+++ index.php
@@ -18,19 +18,25 @@ namespace SRF;
 get_header();
 
 $posts_title = get_the_title( get_option( 'page_for_posts', true ) );
+$archive_subheading = is_category() ? "Category: " : "Tag: ";
 ?>
 
 	<?php if ( have_posts() ) : ?>
 		<div class="<?php echo esc_attr( srf_container_classes() ); ?>">
 			<header class="entry-header max-w-3xl mx-auto mb-16 text-center">
 				<h1 class="entry-title mb-4 text-4xl lg:text-5xl text-gray-600 font-extrabold"><?php echo esc_html( $posts_title ); ?></h1>
-				<div class="mx-auto w-2/3 h-1 bg-gradient-to-r from-blue-400 to-purple-400 rounded transform translate-y-2"></div>
+				<div class="mx-auto mb-6 w-2/3 h-1 bg-gradient-to-r from-blue-400 to-purple-400 rounded transform translate-y-2"></div>
+				<?php
+					if ( is_category() || is_tag() ) { 
+						the_archive_title( '<h4 class="archive-title text-xl lg:text-2xl text-gray-700 font-bold italic">' . $archive_subheading, '</h4>' );
+					}
+				?>
 			</header>
 
 			<?php if ( is_home() || is_category() || is_tag() ) : ?>
-				<div class="prose lg:prose-xl max-w-6xl mx-auto mb-10 flex justify-between flex-wrap">
+				<div class="max-w-6xl mx-auto mb-10 flex justify-between flex-wrap text-lg text-gray-600">
 					<div>
-						<h4 class="text-sm">Filter by Category:</h4>
+						<h4 class="mb-2 font-semibold text-sm text-gray-900">Filter by Category:</h4>
 						<?php 
 							$cat_args = array(
 								'taxonomy'        => 'category',
@@ -44,7 +50,7 @@ $posts_title = get_the_title( get_option( 'page_for_posts', true ) );
 					</div>
 
 					<div>
-						<h4 class="text-sm">Filter by Tag:</h4>
+						<h4 class="mb-2 font-semibold text-sm text-gray-900">Filter by Tag:</h4>
 						<?php
 							$current_category = get_queried_object();
 							$tag_args = array(
