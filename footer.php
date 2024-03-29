10d236f1fd82fa7d8dae498147db41adde78741d
Daniel W. Robert <danielwrobert@gmail.com>
2024-03-29 11:00:59 -0400

    Add medical disclaimer to footer.

--- footer.php
+++ footer.php
@@ -27,23 +27,76 @@ namespace SRF;
 			class="container mx-auto px-6 lg:px-0 space-y-4 md:space-y-0 flex flex-wrap justify-center md:justify-between">
 			<!-- flags (credit: https://github.com/HatScripts/circle-flags) -->
 			<div class="flex space-x-5">
-				<a href="<?php echo esc_url( home_url( '/team/board-members/' ) ); ?>" class="block transition duration-500 ease-in-out filter grayscale hover:grayscale-0">
-					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 512 512"><mask id="a"><circle cx="256" cy="256" r="256" fill="#fff"/></mask><g mask="url(#a)"><path fill="#eee" d="M256 0h256v64l-32 32 32 32v64l-32 32 32 32v64l-32 32 32 32v64l-256 32L0 448v-64l32-32-32-32v-64z"/><path fill="#d80027" d="M224 64h288v64H224Zm0 128h288v64H256ZM0 320h512v64H0Zm0 128h512v64H0Z"/><path fill="#0052b4" d="M0 0h256v256H0Z"/><path fill="#eee" d="m187 243 57-41h-70l57 41-22-67zm-81 0 57-41H93l57 41-22-67zm-81 0 57-41H12l57 41-22-67zm162-81 57-41h-70l57 41-22-67zm-81 0 57-41H93l57 41-22-67zm-81 0 57-41H12l57 41-22-67Zm162-82 57-41h-70l57 41-22-67Zm-81 0 57-41H93l57 41-22-67zm-81 0 57-41H12l57 41-22-67Z"/></g></svg>
+				<a href="<?php echo esc_url( home_url( '/team/board-members/' ) ); ?>"
+				   class="block transition duration-500 ease-in-out filter grayscale hover:grayscale-0">
+					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 512 512">
+						<mask id="a">
+							<circle cx="256" cy="256" r="256" fill="#fff" />
+						</mask>
+						<g mask="url(#a)">
+							<path fill="#eee"
+								  d="M256 0h256v64l-32 32 32 32v64l-32 32 32 32v64l-32 32 32 32v64l-256 32L0 448v-64l32-32-32-32v-64z" />
+							<path fill="#d80027"
+								  d="M224 64h288v64H224Zm0 128h288v64H256ZM0 320h512v64H0Zm0 128h512v64H0Z" />
+							<path fill="#0052b4" d="M0 0h256v256H0Z" />
+							<path fill="#eee"
+								  d="m187 243 57-41h-70l57 41-22-67zm-81 0 57-41H93l57 41-22-67zm-81 0 57-41H12l57 41-22-67zm162-81 57-41h-70l57 41-22-67zm-81 0 57-41H93l57 41-22-67zm-81 0 57-41H12l57 41-22-67Zm162-82 57-41h-70l57 41-22-67Zm-81 0 57-41H93l57 41-22-67zm-81 0 57-41H12l57 41-22-67Z" />
+						</g>
+					</svg>
 				</a>
-				<a href="<?php echo esc_url( home_url( '/team/srf-uk/' ) ); ?>" class="block transition duration-500 ease-in-out filter grayscale hover:grayscale-0">
-					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 512 512"><mask id="a"><circle cx="256" cy="256" r="256" fill="#fff"/></mask><g mask="url(#a)"><path fill="#eee" d="m0 0 8 22-8 23v23l32 54-32 54v32l32 48-32 48v32l32 54-32 54v68l22-8 23 8h23l54-32 54 32h32l48-32 48 32h32l54-32 54 32h68l-8-22 8-23v-23l-32-54 32-54v-32l-32-48 32-48v-32l-32-54 32-54V0l-22 8-23-8h-23l-54 32-54-32h-32l-48 32-48-32h-32l-54 32L68 0H0z"/><path fill="#0052b4" d="M336 0v108L444 0Zm176 68L404 176h108zM0 176h108L0 68ZM68 0l108 108V0Zm108 512V404L68 512ZM0 444l108-108H0Zm512-108H404l108 108Zm-68 176L336 404v108z"/><path fill="#d80027" d="M0 0v45l131 131h45L0 0zm208 0v208H0v96h208v208h96V304h208v-96H304V0h-96zm259 0L336 131v45L512 0h-45zM176 336 0 512h45l131-131v-45zm160 0 176 176v-45L381 336h-45z"/></g></svg>
+				<a href="<?php echo esc_url( home_url( '/team/srf-uk/' ) ); ?>"
+				   class="block transition duration-500 ease-in-out filter grayscale hover:grayscale-0">
+					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 512 512">
+						<mask id="a">
+							<circle cx="256" cy="256" r="256" fill="#fff" />
+						</mask>
+						<g mask="url(#a)">
+							<path fill="#eee"
+								  d="m0 0 8 22-8 23v23l32 54-32 54v32l32 48-32 48v32l32 54-32 54v68l22-8 23 8h23l54-32 54 32h32l48-32 48 32h32l54-32 54 32h68l-8-22 8-23v-23l-32-54 32-54v-32l-32-48 32-48v-32l-32-54 32-54V0l-22 8-23-8h-23l-54 32-54-32h-32l-48 32-48-32h-32l-54 32L68 0H0z" />
+							<path fill="#0052b4"
+								  d="M336 0v108L444 0Zm176 68L404 176h108zM0 176h108L0 68ZM68 0l108 108V0Zm108 512V404L68 512ZM0 444l108-108H0Zm512-108H404l108 108Zm-68 176L336 404v108z" />
+							<path fill="#d80027"
+								  d="M0 0v45l131 131h45L0 0zm208 0v208H0v96h208v208h96V304h208v-96H304V0h-96zm259 0L336 131v45L512 0h-45zM176 336 0 512h45l131-131v-45zm160 0 176 176v-45L381 336h-45z" />
+						</g>
+					</svg>
 				</a>
-				<a href="<?php echo esc_url( home_url( '/team/srf-eu/' ) ); ?>" class="block transition duration-500 ease-in-out filter grayscale hover:grayscale-0">
-					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 512 512"><mask id="a"><circle cx="256" cy="256" r="256" fill="#fff"/></mask><g mask="url(#a)"><path fill="#0052b4" d="M0 0h512v512H0z"/><path fill="#ffda44" d="m256 100.2 8.3 25.5H291l-21.7 15.7 8.3 25.6-21.7-15.8-21.7 15.8 8.3-25.6-21.7-15.7h26.8zm-110.2 45.6 24 12.2 18.9-19-4.2 26.5 23.9 12.2-26.5 4.2-4.2 26.5-12.2-24-26.5 4.3 19-19zM100.2 256l25.5-8.3V221l15.7 21.7 25.6-8.3-15.8 21.7 15.8 21.7-25.6-8.3-15.7 21.7v-26.8zm45.6 110.2 12.2-24-19-18.9 26.5 4.2 12.2-23.9 4.2 26.5 26.5 4.2-24 12.2 4.3 26.5-19-19zM256 411.8l-8.3-25.5H221l21.7-15.7-8.3-25.6 21.7 15.8 21.7-15.8-8.3 25.6 21.7 15.7h-26.8zm110.2-45.6-24-12.2-18.9 19 4.2-26.5-23.9-12.2 26.5-4.2 4.2-26.5 12.2 24 26.5-4.3-19 19zM411.8 256l-25.5 8.3V291l-15.7-21.7-25.6 8.3 15.8-21.7-15.8-21.7 25.6 8.3 15.7-21.7v26.8zm-45.6-110.2-12.2 24 19 18.9-26.5-4.2-12.2 23.9-4.2-26.5-26.5-4.2 24-12.2-4.3-26.5 19 19z"/></g></svg>
+				<a href="<?php echo esc_url( home_url( '/team/srf-eu/' ) ); ?>"
+				   class="block transition duration-500 ease-in-out filter grayscale hover:grayscale-0">
+					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 512 512">
+						<mask id="a">
+							<circle cx="256" cy="256" r="256" fill="#fff" />
+						</mask>
+						<g mask="url(#a)">
+							<path fill="#0052b4" d="M0 0h512v512H0z" />
+							<path fill="#ffda44"
+								  d="m256 100.2 8.3 25.5H291l-21.7 15.7 8.3 25.6-21.7-15.8-21.7 15.8 8.3-25.6-21.7-15.7h26.8zm-110.2 45.6 24 12.2 18.9-19-4.2 26.5 23.9 12.2-26.5 4.2-4.2 26.5-12.2-24-26.5 4.3 19-19zM100.2 256l25.5-8.3V221l15.7 21.7 25.6-8.3-15.8 21.7 15.8 21.7-25.6-8.3-15.7 21.7v-26.8zm45.6 110.2 12.2-24-19-18.9 26.5 4.2 12.2-23.9 4.2 26.5 26.5 4.2-24 12.2 4.3 26.5-19-19zM256 411.8l-8.3-25.5H221l21.7-15.7-8.3-25.6 21.7 15.8 21.7-15.8-8.3 25.6 21.7 15.7h-26.8zm110.2-45.6-24-12.2-18.9 19 4.2-26.5-23.9-12.2 26.5-4.2 4.2-26.5 12.2 24 26.5-4.3-19 19zM411.8 256l-25.5 8.3V291l-15.7-21.7-25.6 8.3 15.8-21.7-15.8-21.7 25.6 8.3 15.7-21.7v26.8zm-45.6-110.2-12.2 24 19 18.9-26.5-4.2-12.2 23.9-4.2-26.5-26.5-4.2 24-12.2-4.3-26.5 19 19z" />
+						</g>
+					</svg>
 				</a>
-				<a href="<?php echo esc_url( home_url( '/team/fondo-de-investigacion-syngap/' ) ); ?>" class="block transition duration-500 ease-in-out filter grayscale hover:grayscale-0">
-					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 512 512"><mask id="a"><circle cx="256" cy="256" r="256" fill="#fff"/></mask><g mask="url(#a)"><path fill="#d80027" d="m0 384 255.8-29.7L512 384v128H0z"/><path fill="#0052b4" d="m0 256 259.5-31L512 256v128H0z"/><path fill="#ffda44" d="M0 0h512v256H0z"/></g></svg>
+				<a href="<?php echo esc_url( home_url( '/team/fondo-de-investigacion-syngap/' ) ); ?>"
+				   class="block transition duration-500 ease-in-out filter grayscale hover:grayscale-0">
+					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 512 512">
+						<mask id="a">
+							<circle cx="256" cy="256" r="256" fill="#fff" />
+						</mask>
+						<g mask="url(#a)">
+							<path fill="#d80027" d="m0 384 255.8-29.7L512 384v128H0z" />
+							<path fill="#0052b4" d="m0 256 259.5-31L512 256v128H0z" />
+							<path fill="#ffda44" d="M0 0h512v256H0z" />
+						</g>
+					</svg>
 				</a>
 			</div>
 			<!-- copyright -->
 			<div class="text-center">
-				<p>Copyright &copy; Syngap Research Fund <?php echo esc_html( date( 'Y' ) ); ?> | All Rights Reserved</p>
-				<p><a href="<?php echo esc_url( home_url( '/privacy-policy/' ) ); ?>" class="underline">Privacy Policy</a> | <a href="<?php echo esc_url( home_url( '/contact/' ) ); ?>" class="underline">Contact Us</a></p>
+				<p>Copyright &copy; Syngap Research Fund <?php echo esc_html( date( 'Y' ) ); ?> | All Rights
+					Reserved</p>
+				<p><a href="<?php echo esc_url( home_url( '/privacy-policy/' ) ); ?>" class="underline">Privacy
+						Policy</a> | <a href="<?php echo esc_url( home_url( '/contact/' ) ); ?>" class="underline">Contact
+						Us</a></p>
+				<p>Medical Disclaimer: <span class="italic">Information on this site is not, nor is it intended to be, medical advice. Please
+						consult your physician with any questions or concerns you may have regarding your health.</span>
+				</p>
 			</div>
 			<!-- links -->
 			<div class="flex space-x-5">
