244aa2e9d5335e7488e633c2862943e1aa284366
Daniel W. Robert <danielwrobert@gmail.com>
2025-01-12 22:44:53 -0500

    Add event calendar template overrides for single event.

---
+++ tribe/events/single-event/title.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Single Event Title Template Part
+ *
+ * Override this template in your own theme by creating a file at:
+ * [your-theme]/tribe/events/single-event/title.php
+ *
+ * See more documentation about our Blocks Editor templating system.
+ *
+ * @link http://evnt.is/1aiy
+ *
+ * @version 4.7.2
+ *
+ */
+?>
+
+<header class="max-w-3xl mx-auto mb-10">
+	<?php the_title( '<h1 class="mb-4 text-4xl lg:text-5xl text-gray-600 font-extrabold">', '</h1>' ); ?>
+	<div class="mx-auto w-2/3 h-1 bg-gradient-to-r from-blue-400 to-purple-400 rounded transform translate-y-2"></div>
+</header>
--- tribe/events/v2/default-template.php
+++ tribe/events/v2/default-template.php
@@ -15,11 +15,13 @@ use Tribe\Events\Views\V2\Template_Bootstrap;
 
 get_header();
 ?>
-	<div class="<?php echo esc_attr( srf_container_classes() ); ?> text-center">
-		<header class="entry-header max-w-3xl mx-auto">
-			<h1 class="entry-title mb-4 text-4xl lg:text-5xl text-gray-600 font-extrabold">Calendar</h1>
-			<div class="mx-auto w-2/3 h-1 bg-gradient-to-r from-blue-400 to-purple-400 rounded transform translate-y-2"></div>
-		</header>
+	<div class="container mx-auto px-6 lg:px-0 py-16 text-center">
+		<?php if ( ! is_singular() ) : ?>
+			<header class="entry-header max-w-3xl mx-auto">
+				<h1 class="entry-title mb-4 text-4xl lg:text-5xl text-gray-600 font-extrabold">Calendar</h1>
+				<div class="mx-auto w-2/3 h-1 bg-gradient-to-r from-blue-400 to-purple-400 rounded transform translate-y-2"></div>
+			</header>
+		<?php endif; ?>
 
 		<div class="max-w-6xl mx-auto">
 			<?php echo tribe( Template_Bootstrap::class )->get_view_html(); ?>
