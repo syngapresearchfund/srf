5683792db57f018f9cbd183564ac116aabf07d45
Daniel W. Robert <danielwrobert@gmail.com>
2025-01-14 10:39:02 -0500

    Add AU template.

--- taxonomy-srf-team-category-srf-au.php
+++ taxonomy-srf-team-category-srf-au.php
@@ -23,24 +23,10 @@ get_header();
 	<?php if ( have_posts() ) : ?>
 	<div class="<?php echo esc_attr( $container_classes ); ?> text-center">
 		<header class="entry-header max-w-3xl mx-auto mb-16">
-			<h1 class="entry-title mb-4 text-4xl lg:text-5xl text-gray-600 font-extrabold">STICHTING* SYNGAP RESEARCH
-				FUND EUROPE</h1>
+			<h1 class="entry-title mb-4 text-4xl lg:text-5xl text-gray-600 font-extrabold"><?php echo esc_html( $current_term->name ); ?></h1>
 			<div
 				class="mx-auto w-2/3 h-1 bg-gradient-to-r from-blue-400 to-purple-400 rounded transform translate-y-2"></div>
 		</header>
-
-		<div class="prose lg:prose-xl mx-auto mb-16">
-			<h3>Mission:</h3>
-			<p>Improve the lives for people living with SYNGAP1 syndrome through research and development of treatments,
-				therapies and support systems in Europe.</p>
-
-			<h3>Important Documents:</h3>
-			<a href="https://drive.google.com/file/d/1HGfEdNhFDKLfW9sVI2pNMp-IOTb1X9kF/view?usp=sharing">Policy
-				Paper</a>
-
-			<h3>Contact us:</h3>
-			<a href="info-eu@syngapresearchfund.com">‍info-eu@syngapresearchfund.org</a>
-		</div>
 
 		<div class="max-w-6xl mx-auto grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2">
 			<?php
@@ -60,28 +46,19 @@ get_header();
 		</div>
 		<div class="max-w-6xl mx-auto mt-14 pt-10 text-center border-t-2 border-gray-200">
 			<?php
-				the_posts_navigation(
-					array(
-						'prev_text' => 'Next Page',
-						'next_text' => 'Previous Page',
-					)
-				);
+			the_posts_navigation(
+				array(
+					'prev_text' => 'Next Page',
+					'next_text' => 'Previous Page',
+				)
+			);
 			?>
 		</div>
 
 		<div class="prose lg:prose-xl mx-auto mb-16">
-			<p>The board of SynGap Research Fund Europe does not receive any income for their roles. The founder covers
-				all operational costs, allowing 100% of donations to go to research.</p>
-
-			<p class="italic">*A stichting is a legal entity established under Dutch law, translating to “foundation” in
-				English. A stichting is a self-contained legal entity with a separate legal personality, which has no
-				members or shareholders and therefore, no recognized owner.</p>
-
-			<img class="w-1/2 mx-auto" src="<?php echo get_template_directory_uri() . '/assets/images/SRF_EU.png'; ?>"
-				alt="SRF Europe logo" />
-			<img class="w-1/2 mx-auto p-1 bg-white"
-				src="<?php echo get_template_directory_uri() . '/assets/images/logo_eurordis_members_rvb.jpg'; ?>"
-				alt="SRF Eurodis logo" />
+			<img class="w-1/2 mx-auto"
+				src="<?php echo get_template_directory_uri() . '/assets/images/srf-au-logo.png'; ?>"
+				alt="SRF AU" />
 		</div>
 	</div>
 		<?php
