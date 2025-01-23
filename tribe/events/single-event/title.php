7db2eb214fc01407a296b3f632203a514513ccf6
Daniel W. Robert <danielwrobert@gmail.com>
2025-01-22 22:33:54 -0500

    Remove center txt alignment on calendar pages.

--- tribe/events/single-event/title.php
+++ tribe/events/single-event/title.php
@@ -14,7 +14,7 @@
  */
 ?>
 
-<header class="max-w-3xl mx-auto mb-10">
+<header class="max-w-3xl mx-auto mb-10 text-center">
 	<?php the_title( '<h1 class="mb-4 text-4xl lg:text-5xl text-gray-600 font-extrabold">', '</h1>' ); ?>
 	<div class="mx-auto w-2/3 h-1 bg-gradient-to-r from-blue-400 to-purple-400 rounded transform translate-y-2"></div>
 </header>
